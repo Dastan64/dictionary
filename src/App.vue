958995292905215b8c2bf9a3244df1cb7e205f11
Dastan <edaz@technodom.kz>
2023-07-13 14:16:53 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="header">
+    <figure class="header__logo-container">
+      <img src="" alt="" class="header__logo" />
+    </figure>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.header {
+  padding: 16px;
+  background-color: var(--background);
+}
+</style>
