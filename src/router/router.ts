3c78bc7b4328c761d3f1abf775a6baaffe81745d
Dastan <edaz@technodom.kz>
2023-07-19 16:01:09 +0600

    changed files for deployment

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,5 +1,5 @@
 import NotFoundThumb from '@/components/NotFoundThumb/NotFoundThumb.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '@/views/Home/Home.vue';
 import Search from '@/views/Search/Search.vue';
 
@@ -19,7 +19,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
